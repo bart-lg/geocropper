6769980fa8d614ec3b83dcba0c6dd4ad01cc6ff9
bart-lg <b.gruchalski@boku.ac.at>
2020-11-20 14:46:15 +0100

    path of s1_cropped.tif corrected

--- geocropper/geocropper.py
+++ geocropper/geocropper.py
@@ -584,7 +584,7 @@ class Geocropper:
                                 print("Symlink created.")
 
                             # create preview image
-                            utils.createPreviewRGImage(str(targetDir / "S1_cropped.tif"), targetDir)
+                            utils.createPreviewRGImage(str(targetDir / "s1_cropped.tif"), targetDir)
 
                             # set date for tile cropped 
                             db.setTileCropped(poiId, tile["rowid"], mainTargetFolder)
