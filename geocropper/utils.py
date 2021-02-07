18ccb5059b6a142f61f56d200679be48110f81e6
Bartholomäus Leon-Gruchalski <b.gruchalski@boku.ac.at>
2021-02-07 15:16:00 +0100

    bugfix in retrieve_scene_classes

--- geocropper/utils.py
+++ geocropper/utils.py
@@ -1414,7 +1414,7 @@ def retrieve_scene_classes(crops_path):
 
                     if ratios == None:
 
-                        scl_image = pyplot.imread(scl_image_path)[:, :, :1]
+                        scl_image = pyplot.imread(scl_image_path)
                         pixels = scl_image.shape[0] * scl_image.shape[1]
 
                         ratios = {}
