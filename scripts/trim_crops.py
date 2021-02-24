ea67cd727d345a29548bf8b3d70c145fc424b3e7
Bartholomäus Leon-Gruchalski <b.gruchalski@boku.ac.at>
2021-02-24 21:22:06 +0100

    updated script

--- scripts/trim_crops.py
+++ scripts/trim_crops.py
@@ -10,4 +10,4 @@ from geocropper import *
 in_path = "/home/bart/21_windturbine/random_crops_w4000_h4000_filtered"
 out_path = "/home/bart/21_windturbine/random_crops_w2000_h2000_filtered"
 
-geocropper.trim_crops(in_path, out_path, 2000, 2000)
+geocropper.trim_crops(in_path, out_path, 2000, 2000, has_subdir=False)
