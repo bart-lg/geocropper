00f2c327e7cfd7c539db44d8c92b80f16b5574b7
Bartholomäus Leon-Gruchalski <b.gruchalski@boku.ac.at>
2021-06-07 19:55:45 +0200

    ASF file name correction

--- geocropper/asfWrapper.py
+++ geocropper/asfWrapper.py
@@ -8,21 +8,26 @@ logger = logging.getLogger('root')
 
 class AsfWrapper:
 
-	def download_S1_tile(self, granule, out_path):
+    def download_S1_tile(self, granule, out_path):
 
-		if config.asfUser != None and config.asfUser != "":
+        if config.asfUser != None and config.asfUser != "":
 
-			command = ["wget", "-c", f"--http-user={config.asfUser}", f"--http-password={config.asfPW}",
-					   f"https://datapool.asf.alaska.edu/GRD_HD/SA/{granule}.zip", "-P", str(out_path)]
-			subprocess.call(command)
+            if granule.endswith(".zip"):
+                download_file = granule
+            else:
+                download_file = f"{granule}.zip"
 
-			path = out_path / (granule + ".zip")
+            command = ["wget", "-c", f"--http-user={config.asfUser}", f"--http-password={config.asfPW}",
+                       f"https://datapool.asf.alaska.edu/GRD_HD/SA/{download_file}", "-P", str(out_path)]
+            subprocess.call(command)
 
-			if path.is_file():
-				logger.info(f"Tile {granule} downloaded from Alaska Satellite Facility.")
-				return True
-			else:
-				return False
+            path = out_path / download_file
 
-		else:
-			return None
+            if path.is_file():
+                logger.info(f"Tile {granule} downloaded from Alaska Satellite Facility.")
+                return True
+            else:
+                return False
+
+        else:
+            return None
